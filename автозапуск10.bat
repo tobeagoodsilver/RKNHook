5b2aedd6c3b57ac1eeb558c69864107e80e95c1b
tobeagoodsilver <mark04041980@gmail.com>
2025-03-20 23:50:31 +0900

    main

--- автозапуск10.bat
+++ автозапуск10.bat
@@ -8,9 +8,9 @@ echo Нажмите любую клавишу, чтобы продолжить �
 pause
 
 set BIN=%~dp0bin\
-set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50100 ^
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="\"%~dp01.txt\"" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
---filter-udp=50000-50100 --ipset=\"%~dp02.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
+--filter-udp=50000-65535 --ipset=\"%~dp02.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
 --filter-tcp=80 --hostlist=\"%~dp01.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
 --filter-tcp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-ttl=3 --dpi-desync-fake-tls-mod=rnd,rndsni,padencap
 
--- автозапуск9.bat
+++ автозапуск9.bat
@@ -9,10 +9,10 @@ pause
 
 set BIN=%~dp0bin\
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
---filter-udp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new
---filter-udp=50000-50099 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new
---filter-tcp=80 --hostlist=\"%~dp01.txt\" --dpi-desync=fakedsplit --dpi-desync-fooling=md5sig --new
---filter-tcp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-ttl=7 --dpi-desync-fake-tls-mod=rnd,rndsni,padencap"
+--filter-udp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-65535 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
+--filter-tcp=80 --hostlist=\"%~dp01.txt\" --dpi-desync=fakedsplit --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-ttl=7 --dpi-desync-fake-tls-mod=rnd,rndsni,padencap
 
 set SRVCNAME=RKNHook
 
