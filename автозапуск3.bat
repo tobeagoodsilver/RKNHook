7d4f354d097bd0b4e3bde78e4a01131e95019104
tobeagoodsilver <mark04041980@gmail.com>
2025-04-24 10:54:01 +0900

    4

--- автозапуск3.bat
+++ автозапуск3.bat
@@ -5,11 +5,14 @@ echo Нажмите любую клавишу, чтобы продолжить �
 pause
 
 set BIN=%~dp0bin\
-set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50100 ^
 --filter-udp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
---filter-udp=50000-65535 --ipset=\"%~dp02.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
+--filter-udp=50000-50100 --ipset=\"%~dp02.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
 --filter-tcp=80 --hostlist=\"%~dp01.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
 --filter-tcp=443 --hostlist=\"%~dp01.txt\" --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern=\"%BIN%tls_clienthello_www_google_com.bin\"
+--filter-udp=443 --ipset="3.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80 --ipset="3.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --ipset="3.txt" --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=\"%BIN%tls_clienthello_www_google_com.bin\"
 
 set SRVCNAME=RKNHook
 net stop %SRVCNAME%
